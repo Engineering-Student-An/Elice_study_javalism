84cb0e1cc1962af6d77f4100e12f44e9b01b842f
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-12 16:05:06 +0900

    [week2] ch1 - 연습문제 Q13

---
+++ src/week2/chapter1/problem13.java
@@ -0,0 +1,16 @@
+package week2.chapter1;
+
+import java.util.Scanner;
+
+public class problem13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
