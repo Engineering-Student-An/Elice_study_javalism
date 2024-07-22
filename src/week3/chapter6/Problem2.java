dee0349d6fa59cdef9df88716355025c21299bf6
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-22 23:04:52 +0900

    [week3] ch6 - 연습문제 Q2 리펙토링

--- src/week3/chapter6/Problem2.java
+++ src/week3/chapter6/Problem2.java
@@ -25,27 +25,24 @@ public class Problem2 {
             System.out.println("패스 " + (i+1) + ":");
             for (int j = n-1; j > i; j--) {
                 if(arr[j-1] > arr[j]) {
-                    swapAndPrint(arr, j-1, true);
+                    print(arr, j-1, true);
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
                     count ++;
                 } else {
-                    swapAndPrint(arr, j-1, false);
+                    print(arr, j-1, false);
                 }
             }
-            for (int j = 0; j < n; j++) {
-                System.out.printf("%4d", arr[j]);
-            }
-            System.out.println();
+            print(arr, -2, false);
         }
 
         System.out.printf("비교를 %d회 했습니다.\n", comp);
         System.out.printf("교환을 %d회 했습니다.", count);
     }
 
-    private static void swapAndPrint(int[] arr, int j, boolean b) {
-        comp ++;
+    private static void print(int[] arr, int j, boolean b) {
+        if(j!=-2) comp ++;
         for (int i = 0; i < arr.length; i++) {
 
             if (i == j+1) {
