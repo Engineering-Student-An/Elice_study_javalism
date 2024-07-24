8034ceeafff7928ef6eb02a506aaeb32d5806bde
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-24 16:11:48 +0900

    [week3] ch6 - 연습문제 Q10

---
+++ src/week3/chapter6/problem10/ShellSortV1.java
@@ -0,0 +1,44 @@
+package week3.chapter6.problem10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ShellSortV1 {
+
+    static int moveCount = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("arr[" + i + "] : ");
+            arr[i] = scanner.nextInt();
+        }
+
+        shellSort(arr, n);
+
+        System.out.println("정렬 후 : " + Arrays.toString(arr));
+
+        System.out.println("moveCount = " + moveCount);
+    }
+
+    private static void shellSort(int[] arr, int n) {
+        for (int h = n/2; h > 0; h/=2) {
+
+            for (int i = h; i < n; i++) {
+                int j;
+                int temp = arr[i];
+                for (j = i-h;  j>=0 && arr[j]>temp ; j-=h) {
+                    moveCount ++;
+                    arr[j+h] = arr[j];
+                }
+                arr[j+h] = temp;
+                moveCount ++;
+            }
+        }
+    }
+}
