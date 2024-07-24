da3ecc67d8e8ef2f6343cfea6fc0987bb724c35a
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-24 16:13:53 +0900

    [week3] ch5 - 연습문제 Q8 수정

--- src/week3/chapter5/Problem8.java
+++ src/week3/chapter5/Problem8.java
@@ -1,59 +1,56 @@
-//package week3.chapter5;
-//
-//import java.util.Scanner;
-//import java.util.Stack;
-//
-//public class Problem8 {
-//
-//    static class Info {
-//        int n;       // 현재 원반 번호
-//        int remain;  // 자기 포함 남은 원반 수
-//        int start;   // 시작 기둥
-//        int end;     // 목표 기둥
-//        int temp;    // 임시 기둥
-//
-//        public Info(int n, int remain, int start, int end, int temp) {
-//            this.n = n;       // 현재 원반 번호
-//            this.remain = remain; // 남은 원반 수
-//            this.start = start;
-//            this.end = end;
-//            this.temp = temp;
-//        }
-//    }
-//
-//    static String[] pole = {"", "A기둥", "B기둥", "C기둥"};
-//
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("하노이의 탑");
-//        System.out.print("원반의 개수 : ");
-//        int n = scanner.nextInt();
-//
-//        hanoi(n, 1, 3, 2);
-//    }
-//
-//    private static void hanoi(int n, int start, int end, int temp) {
-//        Stack<Info> stack = new Stack<>();
-//        stack.push(new Info(n, n, start, end, temp));
-//
-//        while (!stack.isEmpty()) {
-//            Info now = stack.pop();
-//
-//            if (now.remain == 1) {
-//                // 현재 원반을 옮기는 경우
-//                System.out.println("원반[" + now.n + "]을 " + pole[now.start] + "에서 " + pole[now.end] + "으로 옮김");
-//            } else {
-//                // n-1 원반을 start에서 temp로 이동
-//                while(true) {
-//                    if(now.remain == 1) break;
-//                    stack.push(new Info(now.n - 1, now.remain - 1, now.start, now.temp, now.end));
-//                }
-//
-//                // n번 원반을 start에서 end로 이동
-//                stack.push(new Info(now.n, 1, now.start, now.end, now.temp));
-//                // n-1 원반을 temp에서 end로 이동
-//                stack.push(new Info(now.n - 1, now.remain - 1, now.temp, now.end, now.start));
-//            }
-//        }
-//    }
-//}
+package week3.chapter5;
+
+import java.util.Scanner;
+
+public class Problem8 {
+    static void move(int no, int x, int y) {
+        int[] xstk = new int[100];
+        int[] ystk = new int[100];
+        int[] sstk = new int[100];		// 스택
+        int ptr = 0; 						// 스택포인터
+        int sw = 0;
+
+        while (true) {
+            if (sw == 0 && no > 1) {
+                xstk[ptr] = x;
+                ystk[ptr] = y;
+                sstk[ptr] = sw;
+                ptr++;
+                no = no - 1;
+                y = 6  - x - y;
+                continue;
+            }
+
+            System.out.printf("원반[%d]을 %d 기둥에서 %d 기둥으로 이동\n", no, x, y);
+
+            if (sw == 1 && no > 1) {
+                xstk[ptr] = x;
+                ystk[ptr] = y;
+                sstk[ptr] = sw;
+                ptr++;
+                no = no - 1;
+                x = 6  - x - y;
+                if (++sw == 2) sw = 0;
+                continue;
+            }
+            do {
+                if (ptr-- == 0)
+                    return;
+                x  = xstk[ptr];
+                y  = ystk[ptr];
+                sw = sstk[ptr] + 1;
+                no++;
+            } while (sw == 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("원반의 개수 : ");
+        int n = stdIn.nextInt();
+
+        move(n, 1, 3);
+    }
+}
+
